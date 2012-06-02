d900a6c226b87835b61a6260908cf289995b6aa6
Dinesh <dineshdm7@gmail.com>
2012-06-02 18:52:25 +0530

    changing thank you page redirection for contact us form

--- contact_us_lib/freecontactformsettings.php
+++ contact_us_lib/freecontactformsettings.php
@@ -2,7 +2,7 @@
 
 $email_to = "dineshdhoom@gmail.com"; // your email address
 $email_subject = "Contact Form Message"; // email subject line
-$thankyou = "thankyou.htm"; // thank you page
+$thankyou = "contact_us_lib/thankyou.htm"; // thank you page
 
 // if you update the question on the form -
 // you need to update the questions answer below
